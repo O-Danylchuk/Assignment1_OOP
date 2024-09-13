076a15929f29e96e576cce4b97b9a48b96c3d6d2
O-Danylchuk <odanylchuk@kse.org.ua>
2024-09-13 05:49:20 +0300

    specified Airplane class

--- airplane.cpp
+++ airplane.cpp
@@ -1,18 +1,79 @@
 #include <string>
 #include <iostream>
 #include <stdio.h>
-#include <unordered_set>
-#include "airplane.h"
-#include "seat.cpp"
+#include "./headers/airplane.h"
+#include <vector>
+#include <iostream>
 
-Airplane::Airplane(int amOfSeats, int amOfRows) : amOfSeatsInRow(amOfSeats), rows(amOfRows)
+Airplane::Airplane(const int& seatsInRow, const int& seatsAmount)
+         : seatsInRow(seatsInRow), seatsAmount(seatsAmount)
 {
+    for (int i = 1; i < seatsAmount; i++) {
+        for (int j = 0; j <= seatsInRow; j++) {
+            std::string seat = std::string(1, 'A' + j) + std::to_string(i);
+            seats.push_back(std::make_tuple(seat, false));
+        }
+    }
+}
+Airplane::~Airplane() {}
+
+bool Airplane::isSeatAvailable(const std::string& seatNumber) const {
+    for (const auto& seat : seats) {
+        if (std::get<0>(seat) == seatNumber) {
+            return !std::get<1>(seat);
+        }
+    }
+    return false;
 }
 
-Airplane::~Airplane()
-{
+bool Airplane::hasFlight(const std::string& flightNo) const {
+    return flightNo == flightNo;
 }
 
-bool Airplane::IsSeatAvailable(const std::string& seat) const {
-    return bookedFlights.find(seat) == bookedFlights.end();
+
+
+bool Airplane::bookSeat(const std::string& seatNumber) {
+    for (auto& seat : seats) {
+        if (std::get<0>(seat) == seatNumber) {
+            if (!std::get<1>(seat)) {
+                std::get<1>(seat) = true;
+                return true;
+            }
+            return false;
+        }
+    }
+    return false;
+}
+
+bool Airplane::unbookSeat(const std::string& seatNumber) {
+    for (auto& seat : seats) {
+        if (std::get<0>(seat) == seatNumber) {
+            if (std::get<1>(seat)) {
+                std::get<1>(seat) = false;
+                return true;
+            }
+            return false;
+        }
+    }
+    return false;
+}
+bool Airplane::returnSeat(const std::string& seatNumber) {
+    for (auto& seat : seats) {
+        if (std::get<0>(seat) == seatNumber) {
+            if (std::get<1>(seat)) {
+                std::get<1>(seat) = false;
+                return true;
+            }
+            return false;
+        }
+    }
+    return false;
+}
+
+void Airplane::printAvailableSeats() const {
+    for (const auto& seat : seats) {
+        if (!std::get<1>(seat)) {
+            std::cout << "Seat " << std::get<0>(seat) << " is available\n";
+        }
+    }
 }
